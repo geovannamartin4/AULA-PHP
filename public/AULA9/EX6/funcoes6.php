4cef87ff3d9987558d7e461ef102b8ffa33f1b1d
Geovanna <125368602+geovannamartin4@users.noreply.github.com>
2025-09-17 23:28:20 +0000

    php

--- public/AULA9/EX6/funcoes6.php
+++ public/AULA9/EX6/funcoes6.php
@@ -1,13 +1,15 @@
 <?php
-    function calcularMedia($n1, $n2, $n3){
-        $media = ($n1 + $n2 + $n3) / 3; 
+function calcularMedia($n1, $n2, $n3)
+{
+    $media = ($n1 + $n2 + $n3) / 3;
+}
+function verificarSituacao($media, $nome)
+{
+    $nome = "Geovanna";
+    if ($media >= 7) {
+        echo "$nome você foi aprovado com a média: $media";
+    } else {
+        echo "$nome você foi reprovado com a média: $media";
     }
-    function verificarSituacao($media){
-        if($media >= 7){
-            echo "Você foi aprovado";
-        }else{
-            echo "Você foi reprovado";
-        }
-    }
-    calcularMedia("7", "7", "7");
-?>
+}
+verificarSituacao("1", "1", "1");
